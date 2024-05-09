937002545bebda9b0317cda96790180d3f08945d
DoomPI <rslomtev@edu.hse.ru>
2024-05-09 12:49:47 +0300

    минорный фикс

--- feature/divkit-feature/src/androidMain/kotlin/ru/pyroman/news/feature/divkit/imageloader/GlideDivImageLoader.kt
+++ feature/divkit-feature/src/androidMain/kotlin/ru/pyroman/news/feature/divkit/imageloader/GlideDivImageLoader.kt
@@ -9,6 +9,7 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.request.transition.Transition
 import com.yandex.div.core.images.BitmapSource
 import com.yandex.div.core.images.CachedBitmap
