07969012baab86cb92bdebcd2ef319f2467f99f5
DoomPI <rslomtev@edu.hse.ru>
2024-05-13 18:39:02 +0300

    поправил mvp для search custom view в android

--- feature/divkit-custom-feature/src/androidMain/kotlin/ru/pyroman/news/feature/divkit/custom/search/SearchDivCustomViewAdapter.kt
+++ feature/divkit-custom-feature/src/androidMain/kotlin/ru/pyroman/news/feature/divkit/custom/search/SearchDivCustomViewAdapter.kt
@@ -1,24 +1,66 @@
 package ru.pyroman.news.feature.divkit.custom.search
 
 import android.content.Context
-import ru.pyroman.news.feature.divkit.custom.AbstractDivCustomViewAdapter
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import androidx.core.widget.addTextChangedListener
+import com.yandex.div2.DivCustom
+import ru.pyroman.mvpkmp.MvpDelegate.Companion.mvpDelegate
 import ru.pyroman.news.feature.divkit.custom.DivCustomType
+import ru.pyroman.news.feature.divkit.custom.MvpDivCustomViewAdapter
+import ru.pyroman.news.feature.search.databinding.ViewSearchBinding
 import ru.pyroman.news.feature.search.presenter.SearchPresenterFactory
-import ru.pyroman.news.feature.search.view.SearchView
+import ru.pyroman.news.feature.search.view.SearchMvpView
 
 class SearchDivCustomViewAdapter(
     private val context: Context,
     private val searchPresenterFactory: SearchPresenterFactory,
-) : AbstractDivCustomViewAdapter<SearchView>() {
+) : MvpDivCustomViewAdapter<View>(), SearchMvpView {
 
     override val customType = DivCustomType.SEARCH.customType
 
-    override fun createView(): SearchView {
-        val searchView = SearchView(
-            searchPresenterFactory = searchPresenterFactory,
-            context = context,
-        )
+    override val mvpDelegate = mvpDelegate {
+        searchPresenterFactory.create()
+    }
 
-        return searchView
+    private var _binding: ViewSearchBinding? = null
+    private val binding: ViewSearchBinding
+        get() = requireNotNull(_binding)
+
+    private var searchInputTextWatcher: TextWatcher? = null
+
+    override fun createView(): View {
+        return ViewSearchBinding.inflate(LayoutInflater.from(this.context)).apply {
+            _binding = this
+        }.root
+    }
+
+    override fun registerOnSearchInput(onSearchInput: (String) -> Unit) = with(binding) {
+        searchInputTextWatcher = searchTv.addTextChangedListener { searchInput ->
+            onSearchInput(searchInput?.toString().orEmpty())
+        }
+    }
+
+    override fun registerOnCancelClick(onCancelClick: () -> Unit) = with(binding) {
+        cancelIv.setOnClickListener { onCancelClick() }
+    }
+
+    override fun unregisterOnSearchInput() = with(binding) {
+        searchTv.removeTextChangedListener(searchInputTextWatcher)
+        searchInputTextWatcher = null
+    }
+
+    override fun unregisterOnCancelClick() = with(binding) {
+        cancelIv.setOnClickListener(null)
+    }
+
+    override fun clearSearchInput() = with(binding) {
+        searchTv.text = null
+    }
+
+    override fun releaseView(view: View, div: DivCustom) {
+        super.releaseView(view, div)
+        _binding = null
     }
 }
