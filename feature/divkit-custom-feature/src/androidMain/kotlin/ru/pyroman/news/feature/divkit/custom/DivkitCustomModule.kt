0942b23c3f2809140b7a73378ee18858bbfc9174
DoomPI <rslomtev@edu.hse.ru>
2024-05-15 01:38:13 +0300

    фикс некомпила

--- feature/divkit-custom-feature/src/androidMain/kotlin/ru/pyroman/news/feature/divkit/custom/DivkitCustomModule.kt
+++ feature/divkit-custom-feature/src/androidMain/kotlin/ru/pyroman/news/feature/divkit/custom/DivkitCustomModule.kt
@@ -24,7 +24,7 @@ private class DivkitCustomModuleBridgeImpl : DivkitCustomModuleBridge() {
                 SearchDivCustomViewAdapter(
                     context = contextInstance(),
                     searchPresenterFactory = instance("SearchPresenterFactory"),
-                    globalVariableController = instance(),
+                    divVariableController = instance(),
                 )
             }
 
